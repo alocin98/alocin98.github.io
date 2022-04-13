3d3e19e7a03e47702eb7eb8c67676c993e873ab4
Nicolas Mueller <nicolas.mueller2@gmail.com>
2022-04-13 10:06:49 +0200

    multilanguage

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,20 @@ AOS.init({
 });
 
 // Add your javascript here
+
+function setLanguage(locale) {
+  if(locale.includes('de')) $('[lang="en"]').hide();
+  else if(locale.includes('en')) $('[lang="de"]').hide();
+}
+
+function changeLanguage(locale) {
+  if(locale.includes('de')) {$('[lang="en"]').hide(); $('[lang="de"]').show();}
+  else if(locale.includes('en')) {$('[lang="en"]').show(); $('[lang="de"]').hide();}
+}
+
+$('#switch-lang').click(function() {
+  $('[lang="en"]').toggle();
+  $('[lang="de"]').toggle();
+});
+
+setLanguage(navigator.language);
